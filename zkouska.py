d8c21dda8e74aa8488c44b9eec647e79fd0e0f8a
FlexLoL <frantisekpetricek@email.cz>
2024-04-15 19:23:06 +0200

    pridani opakuj funkce

--- zkouska.py
+++ zkouska.py
@@ -2,9 +2,13 @@ print("Hello word")
 def secti(a, b):
     return a + b 
 
+def opakuj(x):
+    for i in len(x):
+        print(i)
 print(secti(5,10))
 
 def vynasob(a, b):
+    opakuj(a)
     for i in len(a):
         c = a * b 
     return c 
